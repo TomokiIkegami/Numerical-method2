163fc2df87dbf76327d2d7b69e9bf1106e0622b2
tomoki <gampsocleis.ussuriensis@gmail.com>
2021-02-14 16:25:34 +0900

    fourth commit

---
+++ cl_4-5-1.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Feb 14 16:13:42 2021
+
+@author: Tomoki Ikegami
+"""
+
+work=15
+
+def func():
+    func_1=1
+    work=21
+    print(func_1)
+    print(work)
+
+func()
+print(work)
